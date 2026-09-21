1ceb9b063aadef678b4880aa4174cdfa652f2642
JonasMahlburg <darrylmanga@yahoo.com>
2026-09-21 09:18:22 +0200

    feat: added example Card - force landscape mode - add Card struct - added first UI for Card - implement CardView

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Jonas Mahlburg on 21.09.26.
+//
+
+import Foundation
+
+struct Card {
+    var prompt: String
+    var answer: String
+    
+    static let example = Card(prompt: "Wie heißt der Schaupieler von John Wick?", answer: "Keanu Reeves")
+}
