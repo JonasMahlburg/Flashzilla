a604e20223753ebc00da50c2860177e2663b39be
JonasMahlburg <darrylmanga@yahoo.com>
2026-09-21 08:54:49 +0200

    Initial Commit

---
+++ Flashzilla/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Flashzilla
+//
+//  Created by Jonas Mahlburg on 31.08.26.
+//
+
+import SwiftUI
+import Combine
+
+struct ContentView: View {
+    
+    var body: some View {
+
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Jonas Mahlburg on 31.08.26.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
