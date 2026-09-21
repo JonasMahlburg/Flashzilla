ad6d03a9841d49dbeadbe76a6ed91ff60ad7b2e8
JonasMahlburg <darrylmanga@yahoo.com>
2026-09-21 09:38:01 +0200

    feat: add UI and View extension - implement Background - adding View extension with 'stacked'-method
    note: 'stacked'-method shows multiple Cards stacked with slightly repositioning

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -6,12 +6,35 @@
 //
 
 import SwiftUI
-import Combine
+
+
 
 struct ContentView: View {
+    @State private var cards = Array<Card>(repeating: .example, count: 10)
     
     var body: some View {
-        CardView(card: .example)
+        ZStack {
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+            VStack {
+                ZStack {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: cards[index])
+                            .stacked(at: index, in: cards.count)
+                    }
+                }
+            }
+        }
+    }
+}
+
+//MARK: - EXTENSIONS
+
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = Double(total - position)
+        return self.offset(y: offset * 10)
     }
 }
 
